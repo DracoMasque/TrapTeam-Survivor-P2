be01c6ab6dac94b9d4dd2a85aa49542007ab74db
DracoMasque <aksel.boulongne@gmail.com>
2026-09-26 20:23:59 +0200

    essai de différente methode
    j'en est trouver une qui peut marchait il faut juste que j'arrive a bien la faire fonctionné

--- Assets/Scripts/Building/BuildingSystem.cs
+++ Assets/Scripts/Building/BuildingSystem.cs
@@ -1,6 +1,41 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class BuildingSystem : MonoBehaviour
 {
-    [SerializeField] public const float cellSize = 20f;
+    private Camera _camera;
+    [SerializeField] private LayerMask placementLayerMask;
+    private InputAction mousePos;
+    private Vector2 lastPos;
+    [SerializeField] private GameObject mouseIndicator, cellIndicator;
+    [SerializeField] private Grid grid;
+
+    void Start()
+    {
+        _camera = Camera.main;
+        mousePos = InputSystem.actions["MousePos"];
+    }
+
+    void Update()
+    {
+        mouseIndicator.transform.position = FollowMouse();
+        Vector3Int gridPos = grid.WorldToCell(FollowMouse());
+        cellIndicator.transform.position = grid.CellToWorld(gridPos);
+    }
+
+    private Vector2 FollowMouse()
+    {
+        /*
+        Ray ray = _camera.ScreenPointToRay(mousePos.ReadValue<Vector3>());
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 1, placementLayerMask))
+        {
+            lastPos = hit.point;
+            print(lastPos);
+        }
+        */
+
+        return _camera.ScreenToWorldPoint(mousePos.ReadValue<Vector2>());
+    }
 }
